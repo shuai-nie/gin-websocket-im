c5dc278a0386be1d59cf4df9940f36286ac25b6d
dave <yesheng35@126.com>
2026-01-05 22:56:36 +0800

    init

--- models/UserBaseic.go
+++ models/UserBaseic.go
@@ -1,5 +1,21 @@
-
+package models
 
 type UserBaseic struct {
 	gorm.Model
+	Name string
+	Password string
+	Phone string
+	Email string
+	Identity string
+	ClientIp string
+	ClientPort string
+	LoginTime uint64
+	HeartbeatTime uint64
+	LogOutTime uint64
+	IsLogout bool
+	DeviceInfo string
+}
+
+func (table *UserBaseic) TableName() string {
+
 }
