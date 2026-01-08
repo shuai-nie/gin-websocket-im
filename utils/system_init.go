54142b6d2f68f21857e3f591dd0bd0cfba00e16c
dave <yesheng35@126.com>
2026-01-08 23:20:24 +0800

    init

--- utils/system_init.go
+++ utils/system_init.go
@@ -1,7 +1,23 @@
 package utils
 
+func InitConfig() {
+	viper.SetConfigName("app")
+	viper.AddConfigPath("ginchat\\config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("config app:", viper.Get("app"))
+	fmt.Println("config mysql:", viper.Get("mysql"))
+}
 
 
+func INitMySQL() {
+	db, err := gorm.Open(mysql.Open(""), &gorn.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
 
 
 
