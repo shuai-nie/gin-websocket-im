7a9ef5d53739f4f09b990611f4474c015ba89220
dave <yesheng35@126.com>
2026-01-11 23:03:10 +0800

    init 添加service

---
+++ service/user_basic.go
@@ -0,0 +1,15 @@
+package service
+
+func GetUserList(c *gin.Context) {
+	userList := make([]*models.UserBasic, 10)
+	userList, err := models.UserBasic{}.List()
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "获取失败",
+		})
+	}
+
+	c.JSON(200, gin.H{
+		"message": userList
+	})
+}
